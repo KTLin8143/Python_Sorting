62f5e3c10ec9f2a847b49e259bc3861cb8cfd7f2
CarterLin <carterlin.iim05g@g2.nctu.edu.tw>
2017-08-04 13:22:13 +0800

    bubble_sort
    version2,
    temp = n[j]
    n[j]=n[j+1]
    n[j+1] = temp
    is equal to
    n[j], n[j + 1], = n[j + 1], n[j]

---
+++ bubble_sortv2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Bubble Sort"""
+
+def bubble_sort(n):
+    for i in range(0, len(n) - 1):  # 總共有n-1回 (n為數字個數)
+        for j in range(0, len(n) - 1 - i):  # 每回所需檢查的個數
+            if n[j] > n[j + 1]:
+                n[j], n[j + 1], = n[j + 1], n[j]
+        print('Round', i + 1, n)
+
+
+mylist = [20, 9, 100, 0, 55, 3, 11]
+print ('Bubble_Sort')
+bubble_sort(mylist)
